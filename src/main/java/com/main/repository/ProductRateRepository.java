dd14606b6a374f9a7b1005bec57e7c58a5ef6b7c
Sahra <114344929+Yennhu0708@users.noreply.github.com>
2023-11-04 18:55:33 +0700

    [BE-DS đánh giá và DS bình luận]

--- src/main/java/com/main/repository/ProductRateRepository.java
+++ src/main/java/com/main/repository/ProductRateRepository.java
@@ -3,9 +3,6 @@ package com.main.repository;
 import com.main.entity.ProductRate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface ProductRateRepository extends JpaRepository<ProductRate, Integer> {
 
-    List<ProductRate>findAllById(int id);
 }
